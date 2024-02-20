f1e269eb0963e960e7a72bad1abab92c7f9e779c
guigomes91 <guilherme.gomes91@outlook.com>
2024-02-20 19:23:36 -0300

    Método merge para atualizar objeto

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -11,6 +11,25 @@ import java.math.BigDecimal;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
+    public void atualizarObjeto() {
+        Produto produto = new Produto();
+        produto.setId(1);
+        produto.setNome("Kindle Paperwhite");
+        //produto.setDescricao("Conheça o novo Kindle");
+        //produto.setPreco(new BigDecimal(599));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        produto = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("Kindle Paperwhite", produto.getNome());
+    }
+
+    @Test
     public void removerObjeto() {
         Produto produto = entityManager.find(Produto.class, 3);
 
