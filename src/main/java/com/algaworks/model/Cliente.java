20ed23c2560442a3aa917b71cd5d6a51b07e29fd
guigomes91 <guilherme.gomes91@outlook.com>
2024-02-23 12:55:42 -0300

    Ajuste do lombock

--- src/main/java/com/algaworks/model/Cliente.java
+++ src/main/java/com/algaworks/model/Cliente.java
@@ -1,31 +1,21 @@
 package com.algaworks.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Cliente {
+
+    @EqualsAndHashCode.Include
     @Id
     private int id;
+
     private String nome;
-
-    public Cliente(){}
-
-    public Cliente(int id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-    @Override
-    public String toString() {
-        return String.format("Código %s - nome %s", this.id, this.nome);
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getId() {
-        return this.id;
-    }
-    public String getNome() {
-        return this.nome;
-    }
 }
