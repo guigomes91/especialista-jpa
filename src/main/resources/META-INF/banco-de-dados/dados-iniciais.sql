e70c1ea34c7272f078f4af38da1a485d5fef874e
guigomes91 <guilherme.gomes91@outlook.com>
2024-02-27 21:09:28 -0300

    Deletando entidade referenciadas

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -3,3 +3,6 @@ insert into produto (id, nome, preco, descricao) values (3, 'Camera GoPro Hero7'
 
 insert into cliente (id, nome) values (1, 'Maria');
 insert into cliente (id, nome) values (2, 'João');
+
+insert into pedido (id, cliente_id, data_pedido, total, status) values (1, 1, sysdate(), 100.0, 'AGUARDANDO');
+insert into item_pedido (id, pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 1, 5.0, 2);
