6c6f541c8e8631a3e58a31d3b4cadeba26d1c1bc
guigomes91 <guilherme.gomes91@outlook.com>
2024-03-24 18:24:17 -0300

    Operacoes em cascata item pedido e pedido

--- src/main/java/com/algaworks/model/Pedido.java
+++ src/main/java/com/algaworks/model/Pedido.java
@@ -51,7 +51,7 @@ public class Pedido extends EntidadeBaseInteger {
     @OneToOne(mappedBy = "pedido")
     private Pagamento pagamento;
 
-    @OneToMany(mappedBy = "pedido", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "pedido", fetch = FetchType.EAGER)
     private List<ItemPedido> itens;
 
     public boolean isPago() {
