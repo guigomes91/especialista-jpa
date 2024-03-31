caf958b0d5e8b1a23d82c34e2f6032c8d7fdd800
guigomes91 <guilherme.gomes91@outlook.com>
2024-03-31 17:19:21 -0300

    Projecao com DTO

---
+++ src/main/java/com/algaworks/model/dto/ProdutoDTO.java
@@ -0,0 +1,14 @@
+package com.algaworks.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProdutoDTO {
+
+    private Integer id;
+    private String nome;
+}
--- src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -3,19 +3,38 @@ package com.algaworks.ecommerce.jpql;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.model.Cliente;
 import com.algaworks.model.Pedido;
-import com.algaworks.model.Produto;
-import lombok.extern.slf4j.Slf4j;
+import com.algaworks.model.dto.ProdutoDTO;
 import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import java.lang.reflect.Type;
 import java.util.List;
 
 public class BasicoJPQLTest extends EntityManagerTest {
+
+    @Test
+    public void projetarOResultadoComDTO() {
+        String jpql = "select new com.algaworks.model.dto.ProdutoDTO(id, nome) from Produto";
+
+        TypedQuery<ProdutoDTO> typedQuery = entityManager.createQuery(jpql, ProdutoDTO.class);
+        List<ProdutoDTO> produtos = typedQuery.getResultList();
+
+        Assert.assertTrue(!produtos.isEmpty());
+        produtos.forEach(produto -> System.out.println(produto.getId() + " - " + produto.getNome()));
+    }
+
+    @Test
+    public void projetarOResultado() {
+        String jpql = "select id, nome from Produto";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertTrue(lista.get(0).length == 2);
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
 
     @Test
     public void selecionarUmAtributoParaRetorno() {
