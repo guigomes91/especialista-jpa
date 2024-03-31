30e02377526711ce737f4054eae375d5e9f496ac
guigomes91 <guilherme.gomes91@outlook.com>
2024-03-31 16:52:33 -0300

    Diferenca entre TypedQuery e Query

--- src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -5,25 +5,37 @@ import com.algaworks.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class BasicoJPQLTest extends EntityManagerTest {
 
     @Test
     public void buscarPorIdentificador() {
-        // Java persistence query language - JPQL
-
-        // JPQL - select p from Pedido p join p.itens i where p.id = 1 and i.precoProduto > 10
-
-        // SQL - select p.* from pedido p join item_pedido i on i.pedido_id = p.id where p.id = 1 and i.preco_produto > 10
-
         TypedQuery<Pedido> typedQuery = entityManager
                 .createQuery("select p from Pedido p where p.id = 1", Pedido.class);
-        //Pedido pedido = typedQuery.getSingleResult();
-        //Assert.assertNotNull(pedido);
 
-        List<Pedido> lista = typedQuery.getResultList();
-        Assert.assertFalse(lista.isEmpty());
+        Pedido pedido = typedQuery.getSingleResult();
+        Assert.assertNotNull(pedido);
+
+        //List<Pedido> lista = typedQuery.getResultList();
+        //Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void mostrarDiferencaQueries() {
+        String jpql = "select p from Pedido p where p.id = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        Pedido pedido1 = typedQuery.getSingleResult();
+        Assert.assertNotNull(pedido1);
+
+        System.out.println("## Query ##");
+
+        Query query = entityManager.createQuery(jpql);
+        Pedido pedido2 = (Pedido) query.getSingleResult();
+        Assert.assertNotNull(pedido2.getCliente());
     }
 }
