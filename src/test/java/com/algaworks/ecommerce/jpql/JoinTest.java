0cd79173a7941ad409ec57a41e970fd6336aaab6
guigomes91 <guilherme.gomes91@outlook.com>
2024-04-03 20:00:02 -0300

    join fetch

--- src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,6 +9,15 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void usarJoinFetch() {
+        String jpql = "select p from Pedido p left join fetch p.pagamento join fetch p.cliente left join fetch p.notaFiscal";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        List<Pedido> pedidos = typedQuery.getResultList();
+        Assert.assertFalse(pedidos.isEmpty());
+    }
 
     @Test
     public void fazerRightJoin() {
