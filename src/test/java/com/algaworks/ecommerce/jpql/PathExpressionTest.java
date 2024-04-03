1661a337ff787502c2b06608b9ec2ea7bdc07dc9
guigomes91 <guilherme.gomes91@outlook.com>
2024-04-03 20:09:25 -0300

    consulta com join fetch em produto pelo pedido

--- src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
@@ -10,6 +10,16 @@ import java.util.List;
 public class PathExpressionTest extends EntityManagerTest {
 
     @Test
+    public void buscarPedidosComProdutoEspecifico() {
+        String jpql = "select p from Pedido p join fetch p.itens i join fetch i.produto where i.produto.id = 1";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
     public void usarPathExpressions() {
         String jpql = "select p.cliente.nome from Pedido p";
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
