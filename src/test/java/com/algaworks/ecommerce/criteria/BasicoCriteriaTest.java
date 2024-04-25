1a02a04ad7dde4a70dbf21b978c6446951d09a6d
guigomes91 <guilherme.gomes91@outlook.com>
2024-04-25 17:55:10 -0300

    Criteria API - joins

--- src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -4,6 +4,7 @@ import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.model.Cliente;
 import com.algaworks.model.Pedido;
 import com.algaworks.model.Produto;
+import com.algaworks.model.dto.ProdutoDTO;
 import org.hibernate.Criteria;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,6 +19,21 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+    @Test
+    public void projetarResultadoDTO() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<ProdutoDTO> criteriaQuery = criteriaBuilder.createQuery(ProdutoDTO.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(criteriaBuilder.construct(ProdutoDTO.class, root.get("id"), root.get("nome")));
+
+        TypedQuery<ProdutoDTO> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<ProdutoDTO> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(dto -> System.out.println("ID: " + dto.getId() + ", Nome: " + dto.getNome()));
+    }
+
     @Test
     public void projetarResultadoTuple() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
