b6ccb0426b1ab307bacfa5dabaa50874fa35576e
guigomes91 <guilherme.gomes91@outlook.com>
2024-04-28 12:19:27 -0300

    Expressoes gt lt com criteria api

--- src/test/java/com/algaworks/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -3,6 +3,8 @@ package com.algaworks.ecommerce.criteria;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.model.Cliente;
 import com.algaworks.model.Cliente_;
+import com.algaworks.model.Produto;
+import com.algaworks.model.Produto_;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,9 +12,63 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void usarMaiorMenor() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery
+                .select(root)
+                .where(criteriaBuilder.greaterThanOrEqualTo(
+                            root.get(Produto_.preco), new BigDecimal("2000.0")),
+                        criteriaBuilder.lessThan(
+                            root.get(Produto_.preco), new BigDecimal("5000.0")));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> produtos = typedQuery.getResultList();
+        Assert.assertFalse(produtos.isEmpty());
+
+        produtos.forEach(produto -> System.out.println("ID: " + produto.getId() + ", Nome: " + produto.getNome() + ", Preco: " + produto.getPreco()));
+    }
+
+    @Test
+    public void usarIsEmpty() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root).where(criteriaBuilder.isEmpty(root.get(Produto_.categorias)));
+        //criteriaQuery.select(root).where(root.get(Produto_.categorias).isNull()); Isso não roda, da problema is null com coleções
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> produtos = typedQuery.getResultList();
+
+        System.out.print("Nome: " + produtos.getFirst().getNome());
+        Assert.assertFalse(produtos.isEmpty());
+    }
+
+    @Test
+    public void usarIsNull() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        //criteriaQuery.select(root).where(root.get(Produto_.foto).isNull());
+        criteriaQuery.select(root).where(criteriaBuilder.isNull(root.get(Produto_.foto)));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> produtos = typedQuery.getResultList();
+
+        System.out.print("Nome: " + produtos.getFirst().getNome());
+        Assert.assertFalse(produtos.isEmpty());
+    }
+
     @Test
     public void usarExpressaoCondicionalLike() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
