4e0cfef808e601cc03efa2483733e3bd968cccf3
guigomes91 <guilherme.gomes91@outlook.com>
2024-05-01 15:20:00 -0300

    funcoes de data com criteria pai

--- src/main/java/com/algaworks/model/PagamentoBoleto.java
+++ src/main/java/com/algaworks/model/PagamentoBoleto.java
@@ -3,6 +3,7 @@ package com.algaworks.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -15,4 +16,7 @@ public class PagamentoBoleto extends Pagamento {
 
     @Column(name = "codigo_barras", length = 100)
     private String codigoBarras;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
 }
