dbfa68d8178c6a1331862156ea5281a2b6008ace
guigomes91 <guilherme.gomes91@outlook.com>
2024-05-10 21:58:10 -0300

    executar query nativa passando parametros

--- src/test/java/com/algaworks/ecommerce/consultasnativas/ConsultaNativaTest.java
+++ src/test/java/com/algaworks/ecommerce/consultasnativas/ConsultaNativaTest.java
@@ -10,8 +10,34 @@ import java.util.List;
 public class ConsultaNativaTest extends EntityManagerTest {
 
     @Test
+    public void executarSQLComParametro() {
+                String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto " +
+                " from produto where id = :id";
+
+        Query query = entityManager.createNativeQuery(sql, Produto.class);
+        query.setParameter("id", 1);
+
+        List<Produto> lista = query.getResultList();
+        lista.stream().forEach(arr -> System.out.println(
+                String.format("Produto => ID: %s, Nome: %s", arr.getId(), arr.getNome())
+        ));
+    }
+
+    @Test
     public void executarSQLRetornandoEntidade() {
-        String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto from produto_loja";
+        //        String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto " +
+//                " from produto_loja";
+
+//        String sql = "select prd_id id, prd_nome nome, prd_descricao descricao, " +
+//                "            prd_data_criacao data_criacao, prd_data_ultima_atualizacao data_ultima_atualizacao, " +
+//                "            prd_preco preco, prd_foto foto " +
+//                " from ecm_produto";
+
+        String sql = "select id, nome, descricao, " +
+                "            null data_criacao, null data_ultima_atualizacao, " +
+                "            preco, null foto " +
+                " from erp_produto";
+
         Query query = entityManager.createNativeQuery(sql, Produto.class);
 
         List<Produto> lista = query.getResultList();
