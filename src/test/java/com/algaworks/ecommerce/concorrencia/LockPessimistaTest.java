3576eae39a31dafad224dc1b98728cf01e76153c
guigomes91 <guilherme.gomes91@outlook.com>
2024-05-30 15:11:39 -0300

    PESSIMISTIC LOCK

--- src/test/java/com/algaworks/ecommerce/concorrencia/LockPessimistaTest.java
+++ src/test/java/com/algaworks/ecommerce/concorrencia/LockPessimistaTest.java
@@ -5,9 +5,10 @@ import org.junit.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.LockModeType;
 import javax.persistence.Persistence;
 
-public class LockOtimistaTest {
+public class LockPessimistaTest {
 
     protected static EntityManagerFactory entityManagerFactory;
 
@@ -35,47 +36,63 @@ public class LockOtimistaTest {
     }
 
     @Test
-    public void usarLockOtimista() {
+    public void usarLockPessimistaLockModeTypePessimisticRead() {
         Runnable runnable1 = () -> {
+            log("Iniciando Runnable 01.");
+
+            String novaDescricao = "Descrição detalhada. CTM: " + System.currentTimeMillis();
+
             EntityManager entityManager1 = entityManagerFactory.createEntityManager();
             entityManager1.getTransaction().begin();
 
-            log("Cliente 01 vai carregar o produto 1.");
-            Produto produto = entityManager1.find(Produto.class, 1);
+            log("Runnable 01 vai carregar o produto 1.");
+            Produto produto = entityManager1.find(
+                    Produto.class, 1, LockModeType.PESSIMISTIC_READ);
 
-            log("Cliente 01 vai esperar por 3 segundos.");
+            log("Runnable 01 vai alterar o produto.");
+            produto.setDescricao(novaDescricao);
+
+            log("Runnable 01 vai esperar por 3 segundo(s).");
             esperar(3);
 
-            log("Cliente 01 vai alterar o produto.");
-            produto.setDescricao("Descrição detalhada.");
-
-            log("Cliente 01 vai confirmar a transação.");
+            log("Runnable 01 vai confirmar a transação.");
             entityManager1.getTransaction().commit();
             entityManager1.close();
+
+            log("Encerrando Runnable 01.");
         };
 
         Runnable runnable2 = () -> {
+            log("Iniciando Runnable 02.");
+
+            String novaDescricao = "Descrição massa! CTM: " + System.currentTimeMillis();
+
             EntityManager entityManager2 = entityManagerFactory.createEntityManager();
             entityManager2.getTransaction().begin();
 
-            log("Cliente 02 vai carregar o produto 1.");
-            Produto produto = entityManager2.find(Produto.class, 1);
+            log("Runnable 02 vai carregar o produto 2.");
+            Produto produto = entityManager2.find(
+                    Produto.class, 1, LockModeType.PESSIMISTIC_READ);
 
-            log("Cliente 02 vai esperar por 1 segundo.");
+            log("Runnable 02 vai alterar o produto.");
+            produto.setDescricao(novaDescricao);
+
+            log("Runnable 02 vai esperar por 1 segundo(s).");
             esperar(1);
 
-            log("Cliente 02 vai alterar o produto.");
-            produto.setDescricao("Descrição massa!");
-
-            log("Cliente 02 vai confirmar a transação.");
+            log("Runnable 02 vai confirmar a transação.");
             entityManager2.getTransaction().commit();
             entityManager2.close();
+
+            log("Encerrando Runnable 02.");
         };
 
         Thread thread1 = new Thread(runnable1);
         Thread thread2 = new Thread(runnable2);
 
         thread1.start();
+
+        esperar(1);
         thread2.start();
 
         try {
@@ -89,7 +106,7 @@ public class LockOtimistaTest {
         Produto produto = entityManager3.find(Produto.class, 1);
         entityManager3.close();
 
-        Assert.assertEquals("Descrição massa!", produto.getDescricao());
+        Assert.assertTrue(produto.getDescricao().startsWith("Descrição massa!"));
 
         log("Encerrando método de teste.");
     }
